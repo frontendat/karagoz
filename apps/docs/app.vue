cc279997e1c6498b8c2f4c109025184016aeccec
mahmoud.aldaas <mahmoud.aldaas@7p-group.com>
2025-02-20 23:21:49 +0100

    add service worker to set headers for crossOriginIsolation

--- apps/docs/app.vue
+++ apps/docs/app.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+onMounted(() => {
+  if (import.meta.dev) return
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(() => {
+        console.log('Service Worker Registered')
+      })
+      .catch((err) => {
+        console.error('Service Worker Registration Failed:', err)
+      })
+  }
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
