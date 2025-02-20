e524a413e4f6f57d7eb0f58e9bca314d30c6418f
mahmoud.aldaas <mahmoud.aldaas@7p-group.com>
2025-02-20 23:27:50 +0100

    revert service worker

--- apps/docs/app.vue
+++ apps/docs/app.vue
@@ -1,19 +1,3 @@
-<script setup lang="ts">
-onMounted(() => {
-  if (import.meta.dev) return
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then(() => {
-        console.log('Service Worker Registered')
-      })
-      .catch((err) => {
-        console.error('Service Worker Registration Failed:', err)
-      })
-  }
-})
-</script>
-
 <template>
   <NuxtLayout>
     <NuxtPage />
