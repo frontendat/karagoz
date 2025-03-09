6e006b8faf8cbcaecc35b5706765c0221f607f0c
mahmoud.aldaas <mahmoud.aldaas@7p-group.com>
2025-03-09 22:08:32 +0100

    panels guide

---
+++ apps/docs/components/content/sandbox/demo/SandboxDemoAltSandbox.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {
+  KrgzEditorTabs,
+  KrgzPreview,
+  provideWebContainer,
+  useSandbox,
+  useSandboxBoot,
+} from '@karagoz/sandbox'
+import type { FileSystemTree } from '@webcontainer/api'
+import { onBeforeUnmount, onMounted } from 'vue'
+
+const { boot, isBooting } = useSandboxBoot()
+provideWebContainer(boot)
+
+const sandbox = useSandbox()
+
+const { data: initialSnapshot } = await useFetch<FileSystemTree>(
+  '/api/snapshot/express',
+)
+
+onMounted(async () => {
+  if (!initialSnapshot.value) return
+  // Ensure injected promise has been resolved
+  const container = await boot
+  // Continue initialisation
+  await container.mount(await initialSnapshot.value)
+  sandbox.setOption('editor', { suppressClose: true })
+  await sandbox.bootstrap()
+  sandbox.editorTabs.open('./public/index.html')
+})
+
+onBeforeUnmount(() => sandbox.container.value?.teardown())
+</script>
+
+<template>
+  <div class="grid grid-cols-2 h-full">
+    <div class="border-r">
+      <KrgzEditorTabs v-if="!isBooting" />
+    </div>
+    <div>
+      <KrgzPreview v-if="!isBooting" />
+    </div>
+  </div>
+</template>
