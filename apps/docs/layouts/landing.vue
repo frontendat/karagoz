a58e5626d09917f4877ef05e508de2a7de9731f1
ma7moudat <frontendat.dev@gmail.com>
2026-05-18 14:07:36 +0200

    fix landing page padding on mobile

--- apps/docs/layouts/landing.vue
+++ apps/docs/layouts/landing.vue
@@ -4,7 +4,7 @@ import DefaultLayout from '~/layouts/default.vue'
 
 <template>
   <DefaultLayout>
-    <main class="container mx-auto">
+    <main class="container mx-auto px-8 md:px-0">
       <slot></slot>
     </main>
   </DefaultLayout>
